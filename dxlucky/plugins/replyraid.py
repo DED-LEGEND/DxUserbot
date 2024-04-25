14f753ca53f341855a5b8e17f711b425415bb955
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-04-25 05:42:25 +0530

    Update replyraid.py

--- dxlucky/plugins/replyraid.py
+++ dxlucky/plugins/replyraid.py
@@ -1,8 +1,8 @@
 from random import choice
 from pyrogram import filters, Client
 from pyrogram.types import Message
-from Romeo.helper.data import RAID, VERIFIED_USERS, GROUP
-from Romeo import SUDO_USER
+from dxlucky.helper.data import RAID, VERIFIED_USERS, GROUP
+from dxlucky import SUDO_USER
 
 ACTIVATE_RLIST = []
 
