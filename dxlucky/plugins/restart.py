acd69bc38162607aab3cfbf0b32095076f250ce0
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-04-25 05:43:33 +0530

    Update restart.py

--- dxlucky/plugins/restart.py
+++ dxlucky/plugins/restart.py
@@ -10,4 +10,4 @@ async def restart(client: Client, message: Message):
     reply = await message.reply_text("**Restarting...**")
     await message.delete()
     await reply.edit_text("Successfully Restarted RomeoBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
-    os.system(f"kill -9 {os.getpid()} && python3 -m Romeo")
+    os.system(f"kill -9 {os.getpid()} && python3 -m dxlucky")
